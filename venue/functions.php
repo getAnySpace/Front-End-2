91379af82167e9a004fe30225d06fa59d38bfdae
Patrick Bartman <pbartman@hawk.iit.edu>
2016-08-25 18:11:32 -0500

    new file:   venue/functions.php 	modified:   venue/index.php

---
+++ venue/functions.php
@@ -0,0 +1,52 @@
+<?php
+	require "../connect.php";
+	$con = new connect('localhost', 'root', 'password', 'cs425');
+
+	function init() {
+		
+	}
+	
+	function getName() {
+		
+	}
+	
+	function getNeighbourhood() {
+		
+	}
+	
+	function getAddress() {
+		
+	}
+	
+	function getAbout() {
+		
+	}
+	
+	/**
+	 * will need IF LENGTH > #, THEN MAKE 2 COLUMNS
+	 * @return arrayed output of <li>'s with amenities
+	 */
+	function getAmenities() {
+		
+	}
+	
+	function getAvailability() {
+		
+	}
+	
+	/**
+	 * @return arrayed output of <li>'s with <img>'s
+	 */
+	function getPhotos() {
+		
+	}
+	
+	/**
+	 * will need IF LENGTH > #, THEN MAKE 2 COLUMNS
+	 * @return arrayed output of <li>'s with rules
+	 */
+	function getRules() {
+		
+	}
+
+?>	
