21a41e02847b5dd2ef57a078de8f1431a9aa8360
luxcapto <luxcapto@gmail.com>
2016-08-31 15:40:34 -0500

    Better looking but broken amenities/style filter

--- js/multiselect.js
+++ js/multiselect.js
@@ -4,36 +4,75 @@
   license: http://www.opensource.org/licenses/mit-license.php
 */
 
-$(".dropdown dt a").on('click', function() {
-  $(".dropdown dd ul").slideToggle('fast');
+/* AMENITIES */
+
+$(".dropdown-amenities dt a").on('click', function() {
+  $(".dropdown-amenities dd ul").slideToggle('fast');
 });
 
-$(".dropdown dd ul li a").on('click', function() {
-  $(".dropdown dd ul").hide();
+$(".dropdown-amenities dd ul li a").on('click', function() {
+  $(".dropdown-amenities dd ul").hide();
 });
 
 function getSelectedValue(id) {
-  return $("#" + id).find("dt a span.value").html();
+  return $("#" + id).find("dt a .hida-amenities span.value").html();
 }
 
 $(document).bind('click', function(e) {
   var $clicked = $(e.target);
-  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+  if (!$clicked.parents().hasClass("dropdown-amenities")) $(".dropdown-amenities dd ul").hide();
 });
 
-$('.mutliSelect input[type="checkbox"]').on('click', function() {
+$('.mutliSelect-amenities input[type="checkbox"]').on('click', function() {
 
-  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-    title = $(this).val() + ",";
+  var title = $(this).closest('.mutliSelect-amenities').find('input[type="checkbox"]').val(),
+    title = $(this).val() + "&nbsp;&nbsp;";
 
   if ($(this).is(':checked')) {
     var html = '<span title="' + title + '">' + title + '</span>';
-    $('.multiSel').append(html);
-    $(".hida").hide();
+    $('.multiSel-amenities').append(html);
+    $(".hida-amenities").hide();
   } else {
     $('span[title="' + title + '"]').remove();
-    var ret = $(".hida");
-    $('.dropdown dt a').append(ret);
+    var ret = $(".hida-amenities");
+    $('.dropdown-amenities dt a').append(ret);
+
+  }
+});
+
+
+/* STYLE */
+
+$(".dropdown-style dt a").on('click', function() {
+  $(".dropdown-style dd ul").slideToggle('fast');
+});
+
+$(".dropdown-style dd ul li a").on('click', function() {
+  $(".dropdown-style dd ul").hide();
+});
+
+function getSelectedValue(id) {
+  return $("#" + id).find("dt a .hida-style span.value").html();
+}
+
+$(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown-style")) $(".dropdown-style dd ul").hide();
+});
+
+$('.mutliSelect-style input[type="checkbox"]').on('click', function() {
+
+  var title = $(this).closest('.mutliSelect-style').find('input[type="checkbox"]').val(),
+    title = $(this).val() + "&nbsp;&nbsp;";
+
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel-style').append(html);
+    $(".hida-style").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida-style");
+    $('.dropdown-style dt a').append(ret);
 
   }
 });
